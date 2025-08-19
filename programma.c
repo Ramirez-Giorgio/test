cf9036113944599db05e31eb649841cdf1d5a099
Ramirez-Giorgio <giorgio.ramirez@outlook.it>
2025-08-19 14:29:16 +0200

    update code tester

--- programma.c
+++ programma.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main() {
-    printf("Hello World");
+    int n,i;
+    printf("Hello there \n");
+    printf("inserisci un numero: ");
+    scanf("%d", &n);
+    i=n*n;
+    printf("il numero al quadrato è %d\n", i);
+
 return 0;
 }
